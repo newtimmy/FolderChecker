fdbafce9cf6d255abf9c1fac9336acc1445a139a
timmr <timmreich@hotmail.de>
2024-03-31 19:37:00 +0200

    get all needed zip files

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import os
 import datetime
-import time
+
+END_OF_FILE_NAME="tbd"
 
 today = datetime.datetime.now().strftime('%Y-%m-%d')
 print(today)
 # today = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
-directory = "F:\\"
+directory = "C:\\test\\"
 
-folders = next(os.walk('.'))[1]
+folders = next(os.walk(directory))[1]
+
+print(folders)
+
+
+def check_for_zip_file(folder):
+    for file in os.listdir(folder):
+        if file.endswith(folder.split("\\")[len(folder.split("\\"))-1] + END_OF_FILE_NAME):
+            print(os.path.join(folder, file))
+            return True
+    return False
+
 
 with open(directory + "folders.txt", "w") as fp:
     for folder in folders:
-        #
-        c_time = os.stat(os.path.join(directory, folder)).st_ctime
+        to_be_checked_folder = os.path.join(directory, folder)
+        print(to_be_checked_folder)
+        c_time = os.stat(to_be_checked_folder).st_ctime
         timestamp_str = datetime.datetime.fromtimestamp(c_time).strftime('%Y-%m-%d')
         print(timestamp_str)
-        if timestamp_str == today:
-            fp.write("%s\n" % folder)
+        if str(timestamp_str) == str(today):
+            if check_for_zip_file(to_be_checked_folder):
+                fp.write("%s %s\n" % (to_be_checked_folder, c_time))
     print("Done")
 
 
